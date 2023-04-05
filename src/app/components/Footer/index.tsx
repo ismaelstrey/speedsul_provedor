527f97197c6ee78edda9124115e17b37990af40c
ismael strey pereira <ismaelstrey@hotmail.com>
2023-04-05 16:39:49 -0300

    update responsive

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -1,6 +1,33 @@
 const Footer = () => {
-    return <footer className="flex flex-col bg-backFooter bg-cover w-full h-[200px] static bottom-0">
+    return <footer className="flex flex-col bg-backFooter bg-cover w-full h-full static bottom-0">
+        <div className="flex relative top-36 flex-col w-screen bg-black h-full justify-center items-center content-center">
+            <div className="flex h-full text-white container min-[700px]:flex-row max-[800px]:flex-col">
+                <div className="w-1/3 text-center  min-[700px]:w-1/3 max-[800px]:w-screen">
+                    <div className="relative -top-24">
+                        <h1 className="text-xl font-extrabold ">Cobertura</h1>
+                        <span>Maquiné</span>
+                    </div>
+                </div>
+                <div className="w-1/3 text-center  min-[700px]:w-1/3 max-[800px]:w-screen relative -top-20">
+                    <div>
+                        <h1 className="text-xl font-extrabold">Cobertura</h1>
+                        <span>Maquiné</span>
+                    </div>
+                </div>
+                <div className=" w-1/3 text-center  min-[700px]:w-1/3 max-[800px]:w-screen relative -top-12 h-full">
+                    <div className="flex flex-col">
+                        <h1 className="text-xl font-extrabold">Contato</h1>
+                        <span className="ml-4 mt-4">WatsApp</span>
+                        <span className="ml-4 mt-4">WatsApp</span>
+                        <span className="ml-4 mt-4">WatsApp</span>
+                        <span className="ml-4 mt-4">WatsApp</span>
+                        <span className="ml-4 mt-4">WatsApp</span>
+                        <span className="ml-4 mt-4">WatsApp</span>
+                    </div>
+                </div>
 
+            </div>
+        </div>
     </footer>
 }
 
--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,9 +1,9 @@
 export const Header = () => {
     return (
-        <div className="flex w-full h-[200px] bg-[url('/img/logoAtivo.svg')] bg-cover">
-            <div className="bg-backLogo bg-cover w-[200px] h-[50px] mt-4 ml-4"></div>
-            <div className="text-white w-full mt-4">
-                <ul className="flex p-4 w-5/6 flex-wrap justify-end gap-8">
+        <div className="flex w-full h-[200px] bg-[url('/img/logoAtivo.svg')] bg-cover min-[380px]:justify-center">
+            <div className="bg-backLogo bg-cover w-[204px] h-[54px] mt-4 ml-4 m-2 "></div>
+            <div className=" md:flex text-white w-[70%] mt-4 min-[380px]:hidden max-[600px]:block justify-end mr-11 ">
+                <ul className="flex p-4 w-3/6 min-[700px]:w-4/6 flex-wrap justify-end gap-8 ">
                     <li><a href="#">Inicio</a></li>
                     <li><a href="#">Produtos</a></li>
                     <li><a href="#">Serviços</a></li>
